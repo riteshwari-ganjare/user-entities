07d4a6b5cbdf0dc4df9a52acc173dfbc281ae332
riteshwari-ganjare <rganjare22@gmail.com>
2023-01-03 15:18:35 +0700

    first commit

---
+++ src/entities/entityBase.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+
+export class EntityBase {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    creation: Date = new Date();
+
+    @Column()
+    modified: Date = new Date();
+
+}
